d27a2a0dd786b4dc0b266b634c4a75fe51dda194
anibalyucra20 <anibalyucrac@gmail.com>
2025-04-28 08:45:17 -0500

    s

--- include/header.php
+++ include/header.php
@@ -15,11 +15,11 @@
         object-fit: cover;
     }
 
-    .ndfHFb-c4YZDc-Wrql6b, .ndfHFb-c4YZDc-Wrql6b-SmKAyb, .ndfHFb-c4YZDc-Wrql6b-DdWCyb-b0t70b, .ndfHFb-c4YZDc-Wrql6b-AeOLfc-b0t70b, .ndfHFb-c4YZDc-GSQQnc-LgbsSe, .ndfHFb-c4YZDc-to915-LgbsSe, .VIpgJd-TzA9Ye-eEGnhe, .ndfHFb-c4YZDc-LgbsSe, .ndfHFb-c4YZDc-Bz112c, .ndfHFb-c4YZDc-DH6Rkf-Bz112c{
+    .ndfHFb-c4YZDc-Wrql6b .ndfHFb-c4YZDc-Wrql6b-SmKAyb .ndfHFb-c4YZDc-Wrql6b-DdWCyb-b0t70b .ndfHFb-c4YZDc-Wrql6b-AeOLfc-b0t70b .ndfHFb-c4YZDc-GSQQnc-LgbsSe .ndfHFb-c4YZDc-to915-LgbsSe .VIpgJd-TzA9Ye-eEGnhe .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-Bz112c .ndfHFb-c4YZDc-DH6Rkf-Bz112c{
         display: none !important;
         opacity: 0% !important;
     }
-    .ndfHFb-c4YZDc-q77wGc, .ndfHFb-c4YZDc-nJjxad-nK2kYb-i5oIFb, .ndfHFb-c4YZDc-nJjxad-m9bMae-LgbsSe, .VIpgJd-TzA9Ye-eEGnhe, .ndfHFb-c4YZDc-LgbsSe, .ndfHFb-c4YZDc-LgbsSe-OWB6Me, .ndfHFb-c4YZDc-Bz112c, .ndfHFb-c4YZDc-LgbsSe, .ndfHFb-c4YZDc-nJjxad-hj4D6d-LgbsSe, .ndfHFb-c4YZDc-nJjxad-S9gUrf, .ndfHFb-c4YZDc-LgbsSe, .ndfHFb-c4YZDc-to915-LgbsSe, .ndfHFb-c4YZDc-nJjxad-bEDTcc-LgbsSe{
+    .ndfHFb-c4YZDc-q77wGc .ndfHFb-c4YZDc-nJjxad-nK2kYb-i5oIFb .ndfHFb-c4YZDc-nJjxad-m9bMae-LgbsSe .VIpgJd-TzA9Ye-eEGnhe, .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-LgbsSe-OWB6Me .ndfHFb-c4YZDc-Bz112c .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-nJjxad-hj4D6d-LgbsSe .ndfHFb-c4YZDc-nJjxad-S9gUrf .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-to915-LgbsSe .ndfHFb-c4YZDc-nJjxad-bEDTcc-LgbsSe{
         display: none !important;
         opacity: 0% !important;
     }
