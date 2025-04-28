9c290fe4b857d0c55b43abb23747a8b8d9557ce2
anibalyucra20 <anibalyucrac@gmail.com>
2025-04-28 09:39:29 -0500

    Update biblioteca.php

--- biblioteca.php
+++ biblioteca.php
@@ -345,7 +345,7 @@ if (!verificar_sesion($conexion) == 1) {
                                     $r_b_ud = mysqli_fetch_array($b_ud);
                                 ?>
                                     <div class="card col-lg-3 col-md-3 col-sm-6 mb-2">
-                                        <iframe src="https://drive.google.com/file/d/<?php echo $rb_material['link_portada']; ?>/preview" frameborder="none" style="width:100%; height:500px; overflow: hidden; " scrolling="no"></iframe>
+                                        <iframe src="https://drive.google.com/file/d/<?php echo $rb_material['link_portada']; ?>/preview" frameborder="none" style="width:100%; height:500px; overflow: hidden; " scrolling="no" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation"></iframe>
                                         
                                         <!--<object data="https://drive.google.com/file/d/<?php echo $rb_material['link_portada']; ?>/preview" type="application/pdf" style="width:100%; height:500px; overflow: hidden; ">
                                             <embed src="https://drive.google.com/file/d/<?php echo $rb_material['link_portada']; ?>/preview" type="application/pdf" style="width:100%; height:500px; overflow: hidden; " />-->
