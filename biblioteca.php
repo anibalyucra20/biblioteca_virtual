24ba903470b2d5d388bcc1d0bbb42251e8781c1b
anibalyucra20 <anibalyucrac@gmail.com>
2025-04-28 09:54:27 -0500

    Update biblioteca.php

--- biblioteca.php
+++ biblioteca.php
@@ -353,7 +353,7 @@ if (!verificar_sesion($conexion) == 1) {
                                         <div style="position: relative;">
                                             <iframe src="https://drive.google.com/file/d/<?php echo $rb_material['link_portada']; ?>/preview" frameborder="none"  sandbox="allow-scripts allow-same-origin" style="width:100%; height:500px; overflow: hidden; " scrolling="no"></iframe>
                                             <div style="width:40px; height:40px; position:absolute; background: #1e1e1f; right:12px; top: 12px;">&nbsp;</div>
-                                            <div style="width:120px; height:40px; position:absolute; background: #1e1e1f; right:100px; top: 300px;">&nbsp;</div>
+                                            <div style="width:120px; height:40px; position:absolute; background: #1e1e1f; right:80px; top: 400px;">&nbsp;</div>
 
                                         </div>
                                         <div class="card-body">
