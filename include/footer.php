9fefe679cccc85a3aaab93c92a0d1c7cf6a66783
anibalyucra20 <anibalyucrac@gmail.com>
2025-04-28 08:50:41 -0500

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -32,5 +32,19 @@
         // El CSS se aplicará después de que todos los recursos estén cargados.
         console.log("Todos los recursos cargados");
         // Aquí puedes agregar tu CSS, si es necesario.
+        var style = document.createElement('style');
+        style.innerHTML = `
+        .ndfHFb-c4YZDc-Wrql6b .ndfHFb-c4YZDc-Wrql6b-SmKAyb .ndfHFb-c4YZDc-Wrql6b-DdWCyb-b0t70b .ndfHFb-c4YZDc-Wrql6b-AeOLfc-b0t70b .ndfHFb-c4YZDc-GSQQnc-LgbsSe .ndfHFb-c4YZDc-to915-LgbsSe .VIpgJd-TzA9Ye-eEGnhe .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-Bz112c .ndfHFb-c4YZDc-DH6Rkf-Bz112c {
+        display: none !important;
+        opacity: 0% !important;
+    }
+
+    .ndfHFb-c4YZDc-q77wGc .ndfHFb-c4YZDc-nJjxad-nK2kYb-i5oIFb .ndfHFb-c4YZDc-nJjxad-m9bMae-LgbsSe .VIpgJd-TzA9Ye-eEGnhe,
+    .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-LgbsSe-OWB6Me .ndfHFb-c4YZDc-Bz112c .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-nJjxad-hj4D6d-LgbsSe .ndfHFb-c4YZDc-nJjxad-S9gUrf .ndfHFb-c4YZDc-LgbsSe .ndfHFb-c4YZDc-to915-LgbsSe .ndfHFb-c4YZDc-nJjxad-bEDTcc-LgbsSe {
+        display: none !important;
+        opacity: 0% !important;
+    }
+  `;
+        document.head.appendChild(style);
     });
 </script>
