d832e1f37372c6e874317b6311c7afbee2227b22
anibalyucra20 <anibalyucrac@gmail.com>
2025-05-28 17:37:00 -0500

    Update index.php

--- login/index.php
+++ login/index.php
@@ -5,8 +5,6 @@
         <meta charset="utf-8" />
         <title>Biblioteca Virtual</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="MyraStudio" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <!-- App favicon -->
